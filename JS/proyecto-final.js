8e5d88cb9bf2aa26a87d793a6e707c3bdb15cf26
Bonaudo <93019653+Bonaudo@users.noreply.github.com>
2023-01-30 13:31:57 -0300

    Proyecto Final ticmas enero 2023
    Proyecto Final ticmas enero 2023

---
+++ JS/proyecto-final.js
@@ -0,0 +1,18 @@
+const grande = document.querySelector('.grande')
+const punto = document.querySelectorAll('.punto')
+
+
+punto.forEach((cadaPunto, i) => {
+    punto[i].addEventListener('click', () => {
+
+        let posicion = i
+        let operacion = posicion * -20
+
+        grande.style.transform = `translateX(${operacion}%`
+
+        punto.forEach((cadaPunto, i) => {
+            punto[i].classList.remove('activo')
+        })
+        punto[i].classList.add('activo')
+    })
+})
